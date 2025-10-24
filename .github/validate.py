47efca3df4f48fba13f14c75a984cad2ea09dcef
Edness <55930127+EdnessP@users.noreply.github.com>
2025-10-24 13:13:05 +0300

    JSON validation script

---
+++ .github/validate.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# Workflow to validate Redump bot .json
+# Written by Edness   v1.0   2025-10-24
+
+import json, os
+
+def validate_json(path):
+    path = os.path.abspath(path)
+    print("Validating", path)
+
+    # validate json formatting
+    with open(path, "r") as file:
+        articles = json.load(file)
+
+    # validate json data itself
+    assert type(articles["articles"]) is dict
+    for k in articles["articles"]:
+        assert type(articles["articles"][k]) is list
+        for v in articles["articles"][k]:
+            assert type(v) is str
+    assert type(articles["strict"]) is dict
+    for k in articles["strict"]:
+        assert type(articles["strict"][k]) is list
+        for v in articles["strict"][k]:
+            assert type(v) is str
+    assert type(articles["exclude"]) is list
+    for v in articles["exclude"]:
+        assert type(v) is str
+
+    print("Articles list is valid!")
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=str, help="path to a file to read")
+    args = parser.parse_args()
+
+    validate_json(args.path)
